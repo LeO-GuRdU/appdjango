7366b9d8df247bcd5ef6d9784d5b2a62476ea836
LeO_GuRdU <leogurdulich@gmail.com>
2021-10-30 12:36:51 -0300

    quinto commit

--- mysite/agenda/models.py
+++ mysite/agenda/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Contactos(models.Model):
+    Nombre = models.CharField(max_length=250)
+    Apellido = models.CharField(max_length=250)
+    Empresa = models.CharField(max_length=250)
+    Telefono = models.IntegerField()
+    Email = models.EmailField()
+
+    def __str__(self):
+        return self.Nombre + " " + self.Apellido
